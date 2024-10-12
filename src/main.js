a17dc8dab92305dbf5184e577c0fad4fafe65785
Oksana Bakhaieva <obahaeva@gmail.com>
2024-10-12 17:28:33 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-const modalOpen = document.querySelector('#modal-open')
-console.log(modalOpen);
+document.querySelector('#modal-open').addEventListener('click', toggleModal);
+document.querySelector('#modal-close').addEventListener('click', toggleModal);
+document.querySelectorAll('.modal-nav-link').forEach(link => {
+    link.addEventListener('click', toggleModal);
+});
 
-// document.querySelector('#modal-open').addEventListener('click', toggleModal);
-// document.querySelector('#modal-close').addEventListener('click', toggleModal);
-// document.querySelectorAll('.modal-nav-link').forEach(link => {
-//     link.addEventListener('click', toggleModal);
-// });
-
-// function toggleModal (e) {
-//     e.preventDefault();
-//     document.querySelector('.modal').classList.toggle('toggleModal');
-// }
+function toggleModal (e) {
+    e.preventDefault();
+    document.querySelector('.modal').classList.toggle('toggleModal');
+}
